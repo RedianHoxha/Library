b02b74a6fa8b34a0b5a3f14e586ad46018cc7148
nefotatari <nefo.tatari@yahoo.com>
2020-05-30 20:23:55 +0200

    Profile

--- HTML/NefoeditProfile.php
+++ HTML/NefoeditProfile.php
@@ -12,14 +12,17 @@
 
     <title>Edit</title>
     <style>
-    #edit{
-      background-image: url("../FOTO/marble.jpg");
-    }
     .profile h4{
       margin-bottom: 6px;
     }
     .form-group{
       margin-right: 70px;
+    }
+    .test {
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
     }
     </style>
 </head>
@@ -31,8 +34,8 @@
     </div>
     <div class="col-sm-8">
      <div class="profile">
-     <h3><i class="fa fa-user-o"></i> YOUR PROFILE</h3>
-       <h4>Pershendetje, ketu mund te beni ndryshimet ne lidhje me te dhenat tuaja!</h4>
+        <h3><i class="fa fa-user-o"></i> YOUR PROFILE</h3>
+        <h4>Pershendetje, ketu mund te beni ndryshimet ne lidhje me te dhenat tuaja!</h4>
      </div>
          <?php $link = mysqli_connect("localhost", "root", "", "library");
         
@@ -45,20 +48,19 @@
                     $row = mysqli_fetch_array($result)
          ?>
     
-     <div class="form-group">
-       <form class="h5 py-4"action='../PHP/editProfile.php?editProfile=<?php echo $row['IdUser'] ?>' method='post'>
-       <label  for="emri">Emri:</label><br>
-       <input class="form-control" type="text" id="emri" name="emri"  value="<?php echo $row['Emer']; ?>" ><br>
-       <label for="mbiemri">Mbiemri:</label><br>
-       <input class="form-control" type="text" id="mbiemri" name="mbiemri"value="<?php echo $row['Mbiemer']; ?>"><br>
-       <label for="username">Username:</label><br>
-       <input class="form-control" type="text" id="username" name="username"value="<?php echo $row['Username']; ?>"><br>
-        <label for="email">Emaili :</label><br>
-        <input class="form-control" type="email" id="email" name="email"value="<?php echo $row['Email']; ?>"><br>
-        <button type="submit" class="btn btn-secondary btn-lg btn-block" name="update">Update</button>
-
+     <div class="form-group test">
+        <form class="h5 py-4"action='../PHP/editProfile.php?editProfile=<?php echo $row['IdUser'] ?>' method='post'>
+          <label  for="emri">Emri:</label><br>
+          <input class="form-control" type="text" id="emri" name="emri"  value="<?php echo $row['Emer']; ?>" ><br>
+          <label for="mbiemri">Mbiemri:</label><br>
+          <input class="form-control" type="text" id="mbiemri" name="mbiemri"value="<?php echo $row['Mbiemer']; ?>"><br>
+          <label for="username">Username:</label><br>
+          <input class="form-control" type="text" id="username" name="username"value="<?php echo $row['Username']; ?>"><br>
+            <label for="email">Emaili :</label><br>
+            <input class="form-control" type="email" id="email" name="email"value="<?php echo $row['Email']; ?>"><br>
+            <button type="submit" class="btn btn-secondary btn-lg btn-block" name="update">Update</button>
         </form>
-    </div>
+     </div>
     </div>
 </div> 
 </body>
--- HTML/dashboard.php
+++ HTML/dashboard.php
@@ -1,63 +1,82 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/17c7570040.js" crossorigin="anonymous"></script>
-    <title>Document</title>
-    <style>
-.container{
-    background-color: #a6a2a2;
-    width:85%;
-    height:100%;
-    margin-left: -20px;
-    margin-top: 0;
-    
-}
-.buttonconatiner{
-background-color:rgb(108, 117, 126);
-margin-bottom: 10%;
-height: 60px;
-border-radius: 25px;
-}
-.formcontainer{
-    padding-top: 40%;
-   
-}
-.button_text{
-text-align: center;
-color:white;
-padding-top: 2%;
-font-size: 187.5%;
-}
-.example_e{
-    color: white;
-    font-size: 100%;
-}
-body{
-    background-image: url("../FOTO/download.jpg");
-}
-
-    </style>
-</head>
-<body>
-    <div class="container position-absolute">
-    <div class="formcontainer">
-    <div class="buttonconatiner">
-        <div class="button_text " ><a class="example_e" href="../HTML/NefoeditProfile.php" name="edit" ><i class="fa fa-pencil"> Profili juaj/modifiko!</i></a> </div>
-    </div>
-    <div class="buttonconatiner">
-        <div class="button_text" ><a class="example_e" href="shfaqwishlist.php" rel="nofollow noopener"><i class="fa fa-heart"> Te preferuarat!</i></a></div><br>
-    </div>
-    <div class="buttonconatiner">
-        <div class="button_text" ><a class="example_e" href="shporta.php" rel="nofollow noopener"><i class="fa fa-shopping-cart"></i> Shporta!</i></a></div><br>
-    </div>
-    <div class="buttonconatiner">
-        <div class="button_text" ><a class="example_e" href="rezervime.php" rel="nofollow noopener"><i class="fa fa-shopping-cart"></i> Shporta!</i></a></div><br>
-    </div>
-</div>
-    </div>
-    
-</body>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/17c7570040.js" crossorigin="anonymous"></script>
+        <style>
+            html, body {
+                height: 100%;
+            }
+            .nav-mask {
+                width: 350px;
+            }
+            .side-nav {
+                background-color:#a6a2a2;
+                position: fixed;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                width: 350px;
+            }
+            .nav-content {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                flex-direction: column;
+                height: 100%;
+            }
+            .test {
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+            }
+            .buttonconatiner{
+                background-color:rgb(108, 117, 126);
+                margin-bottom: 10%;
+                height: 10%;
+                width: 75%;
+                border-radius: 25px;
+            }
+            .button_text{
+                text-align: center;
+                color:white;
+                padding-top: 6%;
+                font-size: 130%;
+            }
+            .example_e{
+                color: white;
+                font-size: 100%;
+            }
+            body{
+                background-image: url("Untitled.png");
+            }
+        </style>
+    </head>
+    <body>
+        <div style="display:flex;">
+            <div class="nav-mask"></div>
+            <div class="side-nav ">
+                <div class="nav-content ">
+                    <div class="buttonconatiner">
+                        <div class="button_text " ><a class="example_e" href="../HTML/NefoeditProfile.php" name="edit" ><i class="fa fa-pencil"> Profili juaj/modifiko!</i></a> </div>
+                    </div>
+                    <div class="buttonconatiner">
+                        <div class="button_text" ><a class="example_e" href="shfaqwishlist.php" rel="nofollow noopener"><i class="fa fa-heart"> Te preferuarat!</i></a></div><br>
+                    </div>
+                    <div class="buttonconatiner">
+                        <div class="button_text" ><a class="example_e" href="shporta.php" rel="nofollow noopener"><i class="fa fa-shopping-cart"></i> Shporta!</i></a></div><br>
+                    </div>
+                    <div class="buttonconatiner">
+                        <div class="button_text" ><a class="example_e" href="rezervime.php" rel="nofollow noopener"><i class="fa fa-shopping-cart"></i> Rezervimet tuaja!</i></a></div><br>
+                    </div>
+                </div>
+            </div>
+            <div class="test">
+                
+            </div>
+        </div>
+    </body>
 </html>
