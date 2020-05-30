ce2aaac65f247a5f9e022b3078cfad3f704e2f02
RedianHoxha <51862189+RedianHoxha@users.noreply.github.com>
2020-05-30 18:40:46 +0200

    Sara
    Search

--- HTML/dhurolexues.php
+++ HTML/dhurolexues.php
@@ -17,7 +17,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8" />
-    <link rel="stylesheet" type="text/css" href="../CSS/dhuroStyleSara.css" />
+    <link rel="stylesheet" type="text/css" href="../CSS/dhuroStyleSaraa.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
---
+++ HTML/searchtest.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<head>
+    <title> Search</title>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+</head>
+<body>
+    <?php $link = mysqli_connect("localhost", "root", "", "library");
+    // Check connection
+    if($link === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+    ?>
+         
+            <div class="row">
+            
+                       <?php $sqlquery="Select * from librari where Sasia > 0 LIMIT 6";
+                        $result = mysqli_query($link, $sqlquery);
+    
+                        while ($row = mysqli_fetch_array($result)) { ?>
+    
+                <div class="col-sm-6">
+                    <div class="properties">
+                        <div id="fotodhepershkrim">
+                            <div id="foto">
+                                <img src="../FOTO/<?php echo $row['Foto']; ?>" />
+                            </div>
+                            <div>
+                                <p> <?php echo $row['Titull']; ?></p>
+                                <p> <?php echo $row['Autor']; ?></p>
+                                <p> <?php echo $row['Cmimi']; ?></p>
+                            </div>
+                        </div>
+    
+                        <div class="butonat">
+                            <button onclick="location.href = 'blerjethjeshte.php?Bli=<?php echo $row['Titull']; ?>'" name="Bli">Bli</button> <br />
+                            <!--para se te bej rezervim ai suhet te rregjistrohet-->
+                            <button onclick="location.href = 'rezervimthjeshte.php?rezervo=<?php echo $row['Titull']; ?>'" name="rezervo">Rezervo</button> <br />
+                            <button onclick="location.href = 'rezervimthjeshte.php?edit=<?php echo $row['Titull']; ?>'">Shto ne Wishlist</button> <br />
+                        </div>
+                    </div>
+                </div>
+                <?php 
+                    } 
+                ?>
+            </div>
+        </div>
+      
+</body>
+
+
+
+</html>
